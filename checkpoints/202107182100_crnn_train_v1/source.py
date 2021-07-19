eca9ed3c65614a96c65379470407f7b9a0630d5d
rkrkrr0101 <qkrrudgh0@naver.com>
2021-07-19 10:47:38 +0900

    0719

--- checkpoints/202107182100_crnn_train_v1/source.py
+++ checkpoints/202107182100_crnn_train_v1/source.py
@@ -19,23 +19,6 @@ logger=tf.get_logger()
 logger.setLevel(logging.ERROR)
 
 # In[2]
-import os
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.layers import Activation,BatchNormalization,Lambda,Input,Conv2D,Bidirectional,LSTM, Concatenate, MaxPooling2D,GlobalAveragePooling2D
-import tensorflow.keras.backend as K
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-from time import time
-import datetime
-import numpy as np
-from generater import crnnGenerator
-import logging
-logger=tf.get_logger()
-logger.setLevel(logging.ERROR)
-
-# In[3]
 def convert_to_onehot(data):
     #Creates a dict, that maps to every char of alphabet an unique int based on position
     alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
@@ -69,9 +52,9 @@ def convert_to_lable(data):
 
 convert_to_lable('abcd453')
 
-# In[4]
+# In[3]
 def crnn():
-    x=image_input=Input(name='image_input',shape=[128,64,1],dtype='float32')
+    x=image_input=Input(name='image_input',shape=(128,64,1),dtype='float32')
     """
     minx=Input(name='minx',shape=[1],dtype='float32')
     miny=Input(name='miny',shape=[1],dtype='float32')
@@ -84,40 +67,41 @@ def crnn():
     
     """
         
-    x=Conv2D(64, (3, 3), strides=(1, 1), activation='relu', padding='same', name='conv1_1')(x)
-    x=MaxPooling2D(pool_size=(2, 2), name='pool1', padding='same')(x)#64 32
+    x=Conv2D(64, (3, 3), strides=(1, 1), activation='relu', padding='same', name='conv1_1')(x)#128 64 64
+    x=MaxPooling2D(pool_size=(2, 2), name='pool1', padding='same')(x)#64 32 64
     
-    x=Conv2D(128, (3, 3), strides=(1, 1), activation='relu', padding='same', name='conv2_1')(x)
-    #x=MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool2', padding='same')(x)# 32 16
+    x=Conv2D(128, (3, 3), strides=(1, 1), activation='relu', padding='same', name='conv2_1')(x)#64 32 128
+    x=MaxPooling2D(pool_size=(2, 2), name='pool2', padding='same')(x)# 32 16 128
 
-    x=Conv2D(256, (3, 3), strides=(1, 1), activation='relu', padding='same', name='conv3_1')(x)
-    x=Conv2D(256, (3, 3), strides=(1, 1), activation='relu', padding='same', name='conv3_2')(x)
-    #x=MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool3', padding='same')(x)# 16 8
+    x=Conv2D(256, (3, 3), strides=(1, 1), activation='relu', padding='same', name='conv3_1')(x)#32 16 256
+    x=Conv2D(256, (3, 3), strides=(1, 1), activation='relu', padding='same', name='conv3_2')(x)#32 16 256
+    x=MaxPooling2D(pool_size=(1, 2), name='pool3', padding='same')(x)# 32 8 256
     
-    x=Conv2D(512, (3, 3), strides=(1, 1), activation='relu', padding='same', name='conv4_1')(x)
+    x=Conv2D(512, (3, 3), strides=(1, 1), activation='relu', padding='same', name='conv4_1')(x)#32 8 512
     x=BatchNormalization(name='batch1')(x)
     
-    x=Conv2D(512, (3, 3), strides=(1, 1), activation='relu', padding='same', name='conv5_1')(x)
+    x=Conv2D(512, (3, 3), strides=(1, 1), activation='relu', padding='same', name='conv5_1')(x)#32 8 512
     
     x=BatchNormalization(name='batch2')(x)
-    #x=MaxPooling2D(pool_size=(2, 2), strides=(1, 2), name='pool5', padding='valid')(x)# 8 4
-    #63 31 512
-    x=aa=Conv2D(512, (2, 2), strides=(1, 1), activation='relu', padding='valid', name='conv6_1')(x)#63 31 512
-    x=keras.layers.Reshape((-1,512))(x)
+    x=MaxPooling2D(pool_size=(1, 2), name='pool5')(x)# 32 4 512
+    
+    x=aa=Conv2D(512, (2, 2), strides=(1, 1), activation='relu', padding='same', name='conv6_1')(x)# 32 4 512
+    x=keras.layers.Reshape(target_shape=(32,2048),name='reshape')(x)#32 2048
+    x=keras.layers.Dense(64,activation='relu',name='dense1')(x)#32 64
     print(aa)
     x=Bidirectional(LSTM(256, return_sequences=True))(x)
-    x=Bidirectional(LSTM(256, return_sequences=True))(x)
+    x=Bidirectional(LSTM(256, return_sequences=True))(x)#32 512
     
     num_classes=36+1
-    x=keras.layers.Dense(num_classes, name='dense1')(x)#알파벳+숫자#1953 36
+    x=keras.layers.Dense(num_classes, name='dense2')(x)#알파벳+숫자#1953 36
     
     x=y_pred= Activation('softmax', name='softmax')(x)#1953 36
     
    # model_pred=keras.models.Model(inputs=[image_input,minx,miny,maxx,maxy],outputs=x)
     model_pred=keras.models.Model(inputs=image_input,outputs=x)
     
-    #maxstringlen=int(y_pred.shape[1])
-    maxstringlen=30
+    maxstringlen=int(y_pred.shape[1])
+    #maxstringlen=100
     
     def ctc_lambda_func(args):
         y_pred,labels, input_length, label_length = args
@@ -138,67 +122,44 @@ def crnn():
 
     
 
-# In[5]
+# In[4]
 traincrnn,predcrnn,inputlength=crnn()
+traincrnn.summary()
+
+# In[5]
+inputlength
 
 # In[6]
-cropvalpath='D:/ocr/croptestimage/'
-croptrainpath='D:/ocr/croptestvalimage/'
-cropvaljson='D:/ocr/valcrnn.json'
+cropvalpath='D:/engocr/croptestimage/'
+croptrainpath='D:/engocr/croptestvalimage/'
+cropvaljson='D:/engocr/valcrnn.json'
 batch=32
 traingen=crnnGenerator(imgpath=croptrainpath,labelpath=cropvaljson,imgw=128,imgh=64,batch_size=batch,
-                      maxlen=inputlength,inputlen=inputlength)
+                      maxlen=12,inputlen=inputlength)
 print('trainstart')
 traingen.build_data()
 print('valstart')
 valgen=crnnGenerator(imgpath=cropvalpath,labelpath=cropvaljson,imgw=128,imgh=64,batch_size=batch,
-                      maxlen=inputlength,inputlen=inputlength)
+                      maxlen=12,inputlen=inputlength)
 valgen.build_data()
 print('valend')
 
 # In[7]
-traincrnn.compile(optimizer='adam',loss={'ctcloss':lambda labels,y_pred:y_pred})
+"""
+import winsound as sd
+
+def beepsound():
+    fr = 2000    # range : 37 ~ 32767
+    du = 1000     # 1000 ms ==1second
+    sd.Beep(fr, du) # winsound.Beep(frequency, duration)
+beepsound()
+"""
+inputlength
 
 # In[8]
-from tensorflow.python.keras.callbacks import TensorBoard  ## TensorBoard 를 import합니다.
-modelver='crnn_train_v1'
-checkdir = './checkpoints/' + datetime.datetime.now().strftime('%Y%m%d%H%M') + '_' + modelver
-if not os.path.exists(checkdir):
-    os.makedirs(checkdir)
-
-with open(checkdir+'/source.py','wb') as f:
-    source = ''.join(['# In[%i]\n%s\n\n' % (i, In[i]) for i in range(len(In))])
-    f.write(source.encode())
-log_dir = "./logs/fit/"+ datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-tensorboard =TensorBoard(log_dir=log_dir,histogram_freq=1)
+traincrnn.compile(optimizer=tf.keras.optimizers.Adadelta() ,loss={'ctcloss':lambda y_true,y_pred:y_pred})
 
 # In[9]
-#[image_input,labels,input_length,label_length]
-"""
-his=traincrnn.fit(x=[valdata,valdata.labels,np.array( inputlengthlist),np.array(vallabellength)],
-              y=valdata.labels,
-                batch_size=16, 
-              epochs=15,
-              callbacks=[keras.callbacks.ModelCheckpoint(checkdir+'/weights.{epoch:03d}.h5', verbose=1, save_weights_only=True),
-                         tensorboard]
-             )
-"""
-his=traincrnn.fit_generator(generator=traingen.next_batch(),
-                            steps_per_epoch=int(traingen.imgcount/batch),
-                            epochs=2,
-                            callbacks=[keras.callbacks.ModelCheckpoint(checkdir+'/weights.{epoch:03d}.h5', verbose=1, moniter='loss'),
-                            tensorboard],
-                            validation_data=valgen.next_batch(),
-                            validation_steps=int(valgen.imgcount/batch)
-                            
-                           
-                           
-                           )
-
-# In[10]
-traincrnn.compile(optimizer='adam',loss={'ctcloss':lambda labels,y_pred:y_pred})
-
-# In[11]
 from tensorflow.python.keras.callbacks import TensorBoard  ## TensorBoard 를 import합니다.
 modelver='crnn_train_v1'
 checkdir = './checkpoints/' + datetime.datetime.now().strftime('%Y%m%d%H%M') + '_' + modelver
--- generater.py
+++ generater.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 import os, random
@@ -11,7 +11,7 @@ import json
 import pandas as pd
 
 
-# In[3]:
+# In[2]:
 
 
 def convert_to_lable(data):
@@ -20,7 +20,7 @@ def convert_to_lable(data):
 convert_to_lable('afsfs')
 
 
-# In[39]:
+# In[7]:
 
 
 class crnnGenerator:
@@ -34,7 +34,7 @@ class crnnGenerator:
         self.index=list(range(self.imgcount))
         self.curindex=0
         self.maxlen=maxlen
-        self.imgs=np.zeros((self.imgcount,self.imgh,imgw))
+        self.imgs=np.zeros((self.imgcount,self.imgh,self.imgw))
         self.texts=[]
         self.labelpath=labelpath
         self.labelpd=pd.read_json(self.labelpath,orient='index')
@@ -42,18 +42,22 @@ class crnnGenerator:
     
     def build_data(self):
         for i,file in enumerate(self.imgdir):
-            img=cv2.imread(self.imgpath+file,cv2.IMREAD_GRAYSCALE)
-            img=cv2.resize(img,(self.imgw,self.imgh))
-            img=img.astype(np.float32)
-            img=img/255.0
-            
-            self.imgs[i,:,:]=img
-            
             a=self.labelpd.loc[self.labelpd['cropimgid']==file[0:-4] ]['label']
+            if (len(a.values[0])) <self.maxlen:
+                img=cv2.imread(self.imgpath+file,cv2.IMREAD_GRAYSCALE)
+                img=cv2.resize(img,(self.imgw,self.imgh))
+                img=img.astype(np.float32)
+                img=(img / 255.0) * 2.0 - 1.0
 
-            
-            self.texts.append(convert_to_lable(a.values[0]))
-            
+                self.imgs[i,:,:]=img
+
+                
+
+
+                self.texts.append(convert_to_lable(a.values[0]))
+            else:
+                self.imgcount=self.imgcount-1
+        self.index=list(range(self.imgcount)) 
     def next_sample(self):
         self.curindex+=1
         if self.curindex>=self.imgcount:
@@ -64,26 +68,31 @@ class crnnGenerator:
         b=self.texts[self.index[self.curindex]]
         return a,b
     
-    def test(self,i=1):
-        for i,file in enumerate(self.imgdir):
-            a=file
-            break;
-            
-        return a
+    def test(self,i=1,j=2):
+        a=self.imgs[self.index[self.curindex]]
+        b=self.texts[self.index[self.curindex]]
+        return a,b
+    
     def texttest(self,file='000adfe5b817011c_1'):
-        a=self.labelpd.loc[self.labelpd['cropimgid']==file ]['label']
-        self.texts.append(a.get(file))
-        return a.get(file)
+        img=cv2.imread(self.imgpath+file+'.jpg',cv2.IMREAD_GRAYSCALE)
+
+        img=cv2.resize(img,(self.imgw,self.imgh))
+        img=img.astype(np.float32)
+        img=img/255.0
+        cv2.imshow('ada',img)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
     
     def next_batch(self):
         while True:
             xdata=np.ones([self.batch_size,self.imgw,self.imgh,1])
-            ydata=np.full([self.batch_size,self.maxlen],999)
-            inputlength=np.ones((self.batch_size,1))*self.inputlen #최대길이
+            ydata=np.ones([self.batch_size,self.maxlen])
+            inputlength=np.ones((self.batch_size,1))*self.inputlen   #(self.imgw//2) 최대길이
             labellength=np.zeros((self.batch_size,1))
             
             for i in range(self.batch_size):
                 img,text=self.next_sample()
+                
                 img=img.T
                 img=np.expand_dims(img,-1)
                 xdata[i]=img
@@ -107,37 +116,38 @@ class crnnGenerator:
         
 
 
-# In[24]:
+# In[5]:
 
 
 """
-cropvalpath='D:/ocr/abcd/'
-croptrainpath='D:/ocr/abcd/'
-cropvaljson='D:/ocr/valcrnn.json'
+cropvalpath='D:/engocr/abcd/'
+croptrainpath='D:/engocr/abcd/'
+cropvaljson='D:/engocr/valcrnn.json'
 batch=32
-traingen=crnnGenerator(imgpath=croptrainpath,labelpath=cropvaljson,imgw=30,imgh=30,batch_size=batch,
+traingen=crnnGenerator(imgpath=croptrainpath,labelpath=cropvaljson,imgw=128,imgh=64,batch_size=batch,
                       maxlen=100,inputlen=100)
 print('trainstart')
 traingen.build_data()
 print('valstart')
-valgen=crnnGenerator(imgpath=cropvalpath,labelpath=cropvaljson,imgw=30,imgh=30,batch_size=batch,
+valgen=crnnGenerator(imgpath=cropvalpath,labelpath=cropvaljson,imgw=128,imgh=64,batch_size=batch,
                       maxlen=100,inputlen=100)
 valgen.build_data()
 print('valend')
-traingen.next_batch()
+
 """
 
 
-# In[15]:
+# In[6]:
 
 
+#valgen.test()
+#valgen.texttest()
 
 
-
-# In[18]:
-
+# In[7]:
 
 
+#ada=cv2.resize(img,(self.imgw,self.imgh))
 
 
 # In[36]:
